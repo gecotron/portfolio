d9b0b6a99c9ea7ef759b2f379d7eef1a911b9fb6
Gecotron <gecotron@gmail.com>
2022-03-22 16:16:32 -0400

    Big Improvement
    I've made some big changes now, I learned enough Javascript to limit the portfolio to one page using ids to swap the image when the corresponding button is pressed, and made the website a bit darker. Also changed the menu and added buttons

---
+++ Css/main.js
@@ -0,0 +1,24 @@
+function Emoji() {
+  document.getElementById('imgID').src='images/Emoji.png'
+  document.getElementById('descID').innerHTML="Custom emoji made in photoshop, using selection, fill, erase, draw and cut and paste tools"
+}
+function FruitFace() {
+  document.getElementById('imgID').src="images/FruitFace.png"
+  document.getElementById('descID').innerHTML="Made with photoshop using various stock images and the select, transform and move tool."
+}
+function PlaywthText() {
+  document.getElementById('imgID').src='images/PlayingwthText.png'
+  document.getElementById('descID').innerHTML="I've heard that Other M was a bad game, But I liked this quote alot, so I thought I would put it in"
+}
+function HeadSpace() {
+  document.getElementById('imgID').src='images/HeadSpace.jpg'
+  document.getElementById('descID').innerHTML="This was a lot of fun to make, it was very fun to find the stuff that fills my head, <br> but I had to stop myself in the end, because there would be too much to see"
+}
+function Movie() {
+  document.getElementById('imgID').src='images/MovieMashup.png'
+  document.getElementById('descID').innerHTML="This is probably my favourite piece, it took alot of work, but it was fun to do, and I really like the end result, even though its not perfect"
+}
+function Extra() {
+  document.getElementById('imgID').src='images/StampExtra.png'
+  document.getElementById('descID').innerHTML="Some extra work I made in my spare time"
+}
